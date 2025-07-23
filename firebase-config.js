3dc3775498ca4026ded08eff0f3b106c4d3f45e3
haidar-ahmady <ahmadyhilmy@gmail.com>
2025-07-23 08:29:09 +0700

    Initial Commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkAu0P-MtXU5s2V5sEcv7FAONoh9pcvY8",
+  authDomain: "k2q-inventory-management.firebaseapp.com",
+  projectId: "k2q-inventory-management",
+  storageBucket: "k2q-inventory-management.firebasestorage.app",
+  messagingSenderId: "533733293285",
+  appId: "1:533733293285:web:9e6f88f9f1d6616f35a80c",
+  measurementId: "G-M7N0PZHEMM"
+};
